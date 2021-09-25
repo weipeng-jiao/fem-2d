9387f171450833b0594bb958cfed83ddcf8fe9b6
weipeng <weipeng.x.jiao@gmail.com>
2021-09-25 22:01:50 +0800

    debug

--- mian.py
+++ mian.py
@@ -1,26 +1,31 @@
 #author:tony jiao
-#finished
+#finished 4/2019 at HNUT
 
 
+from matplotlib.tri.triinterpolate import LinearTriInterpolator
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 import matplotlib.tri as tri
 import sys
-import slover
+from solver import KMat, gauss, Strain, Stress
 
 ##前处理部分
-x = [0,0,1,2,2,3,3]
-y = [0,2,3,2,1,1,0]
-N=len(x)
-nodes=np.zeros([N,2])
 
-#节点生成网格编号
-triObj = tri.Triangulation(x, y)	# 生成给定平面点组凸包的 Delauney 三角剖分.
+elements= [ [3 , 1 , 0] , [ 2 , 1 , 3 ] ] 
+nodes = np.array( [ [ 0 , 0 ] , [ 1 , 0 ] , [ 1 , 1 ] , [ 0 , 1 ] ])
+F =  [ 0 , 0 , -10000 , 0 ,-10000 , 0 ,0 , 0 ] 
+U = [ [ 1 , 0 ] , [ 2 , 0 ] , [ 7 , 0 ] , [ 8 , 0 ] ] 
+
+x=nodes[:,0]
+y=nodes[:,1]
+N=len(x)
+
+#非结构网格节点生成网格编号
+triObj = tri.Triangulation(x, y,elements)	# 生成给定平面点组凸包的 Delauney 三角剖分.
 print(triObj.triangles) #输出每个单元的顶点编号
 print(triObj.edges) #输出每个边的顶点编号
-elements=np.array(triObj.triangles)
-
+elements=np.array(elements)
 #网格结构可视化
 fig=plt.figure('网格结构图') #生成画图窗口
 sub=fig.add_subplot(111) #调用子图
@@ -31,17 +36,21 @@ sub.scatter(x,y,marker='o',color='b',s=50) #画节点
 for i in range(N): #节点标号
     sub.text(x[i]+0.02,y[i]+0.02,'{:d}'.format(i),fontsize=12,color='b')
 for i in range(len(elements)): #网格标号
-    centx=(x[elements[i,0]]+x[elements[i,1]]+x[elements[i,2]])/3
-    centy=(y[elements[i,0]]+y[elements[i,1]]+y[elements[i,2]])/3
+    centx=(x[elements[i][0]]+x[elements[i][1]]+x[elements[i][2]])/3
+    centy=(y[elements[i][0]]+y[elements[i][1]]+y[elements[i][2]])/3
     sub.text(centx,centy,'{:d}'.format(i),fontsize=12,color='r')
 sub.triplot(triObj,color='k')#画三角形网格
-plt.show()#显示
+
 
 
 #求解部分
-Kt=slover.KMat(elements, nodes, 200000000000,0.25,1)
-X=slover.gauss(Kt,load,F)
-
+E0=200000000000#杨氏模量
+v=0.25#泊松比
+t=1#平板厚度
+kt=KMat(elements, nodes, E0,v,t)#总刚矩阵
+u=gauss(kt, U, F)#位移列阵
+B=Strain(elements,nodes,u)#应变矩阵
+S=Stress(B, E0, v)#应力矩阵
 
 
 
